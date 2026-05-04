fcfce5d2cb9d7017b4b42bc61f492fc194a1a541
Nahla Thabet <nahla.thabet@hypercell.net>
2026-05-04 14:57:30 +0300

    Add IAM role and policy attachment for EBS CSI driver

---
+++ eks-cluster/modules/eks/IAM.tf
@@ -0,0 +1,28 @@
+#IAM role for EBS CSI
+
+resource "aws_iam_role" "ebs_csi_role" {
+  name = "AmazonEKS_EBS_CSI_Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+        }
+      }
+    }]
+  })
+}
+
+#Attach AWS managed policy
+resource "aws_iam_role_policy_attachment" "ebs_csi_attach" {
+  role       = aws_iam_role.ebs_csi_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
