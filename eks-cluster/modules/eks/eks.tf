0a4becf5f2687bfefd2a342c2ed978f16f9af830
Nahla Thabet <nahla.thabet@hypercell.net>
2026-05-20 01:13:39 +0300

    fix(eks): set bootstrap_self_managed_addons to false for EKS cluster

--- eks-cluster/modules/eks/eks.tf
+++ eks-cluster/modules/eks/eks.tf
@@ -15,7 +15,7 @@ resource "aws_eks_cluster" "cluster" {
     endpoint_private_access = true
     endpoint_public_access  = true
   }
-
+  bootstrap_self_managed_addons = false
   # Control plane log types sent to CloudWatch Logs.
   # api:           all kubectl requests to the API server
   # audit:         who did what to which resource (important for security)
